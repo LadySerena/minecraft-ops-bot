1b0e77d6b77fdb64451624a6cc26e85bc5a31a11
LadySerena <serena.tiede+github@gmail.com>
2021-04-20 22:30:34 -0500

    feat: add response to interaction
    This is still hella broke tho so i still gotta mess with it some more. The goal
    was to imitate the sample code here
    https://discord.com/developers/docs/interactions/slash-commands#responding-to-an-interaction
    but i think there's some auth error or something.

--- cmd/discord-bot/main.go
+++ cmd/discord-bot/main.go
@@ -20,7 +20,7 @@ const (
 )
 
 var (
-	publicKey = []byte{}
+	publicKey   = []byte{}
 	bindAddress = serverBindDefault
 )
 
@@ -28,10 +28,23 @@ type discordPing struct {
 	MessageType int `json:"type"`
 }
 
+type InteractionResponse struct {
+	ResponseType int          `json:"type"`
+	Data         CallbackData `json:"data"`
+}
+
+type CallbackData struct {
+	TextToSpeech    bool                             `json:"tts"`
+	Content         string                           `json:"content"`
+	Embeds          []discordgo.MessageEmbed         `json:"embeds"`
+	AllowedMentions discordgo.MessageAllowedMentions `json:"allowed_mentions,omitempty"`
+	Flags           int                              `json:"flags,omitempty"`
+}
+
 func ping(w http.ResponseWriter, r *http.Request) {
-	zap.S().Info("got request")
+	zap.S().Debug("got request")
 	if !discordgo.VerifyInteraction(r, publicKey) {
-		zap.S().Info("could not verify request")
+		zap.S().Error("could not verify request")
 		w.WriteHeader(http.StatusUnauthorized)
 		return
 	}
@@ -53,28 +66,56 @@ func ping(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if parsedBody.MessageType == 1 {
+		zap.S().Debug("responding to ping")
 		w.WriteHeader(http.StatusOK)
 		w.Write(body) // return the body since we are supposed to reply with {"type": 1}
+		return
+	} else {
+		zap.S().Debug("processing real request")
+		w.WriteHeader(http.StatusOK)
+		response := InteractionResponse{
+			ResponseType: 4,
+			Data: CallbackData{
+				TextToSpeech: false,
+				Content:      "meow",
+				Embeds:       make([]discordgo.MessageEmbed, 0),
+				AllowedMentions: discordgo.MessageAllowedMentions{
+					Parse: make([]discordgo.AllowedMentionType, 0),
+				},
+				Flags: 0,
+			},
+		}
+		data, marshalErr := json.Marshal(response)
+
+		if marshalErr != nil {
+			zap.S().Errorf("could not marshal response: %v", marshalErr)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		zap.S().Debugf("struct: %#v", response)
+		zap.S().Debugf("json: %s", string(data))
+		_, writeErr := w.Write(data)
+		if writeErr != nil {
+			zap.S().Errorf("failed to write reply: %v", writeErr)
+		}
 		return
 	}
 
 	w.WriteHeader(http.StatusBadRequest)
 }
 
-
-
 func main() {
 	viper.SetDefault(serverBindEnv, serverBindDefault)
 	viper.AutomaticEnv()
 	bindAddress = viper.GetString(serverBindEnv)
 	//publicKey = []byte(viper.GetString(publicKeyEnv)) // for the verify interaction function the key must be a byte slice
-	logger, loggerInitErr := zap.NewProduction()
+	logger, loggerInitErr := zap.NewDevelopment()
 	if loggerInitErr != nil {
 		log.Fatalf("could not initialize zap logger: %v", loggerInitErr)
 	}
 	zap.ReplaceGlobals(logger)
 
-	zap.S().Infof("public key for verifying requests: %v", publicKeyString)
+	zap.S().Debugf("public key for verifying requests: %v", publicKeyString)
 
 	publicKeyTemp, decodeErr := hex.DecodeString(publicKeyString)
 	if decodeErr != nil {
